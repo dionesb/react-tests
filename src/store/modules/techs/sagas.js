745177bd49893ce97cff0afed4fadeff9b142eb8
dionesb <diones.breda@gmail.com>
2019-08-22 21:54:07 -0300

    Coverage report.

--- src/store/modules/techs/sagas.js
+++ src/store/modules/techs/sagas.js
@@ -1,7 +1,7 @@
 import { call, put } from 'redux-saga/effects';
 import api from '../../../services/api';
 
-import { getTechsSuccess, getTechsFailure } from './actions;';
+import { getTechsSuccess, getTechsFailure } from './actions';
 
 export function* getTechs() {
   try {
